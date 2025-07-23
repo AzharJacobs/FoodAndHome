4c256e1630758317a64dc33593ef0abf92f44c31
AzharJacobs <azharj2003@gmail.com>
2025-07-23 09:37:25 +0200

    build-fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer'; // optional
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true }) // optional: opens a report in browser after build
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    chunkSizeWarningLimit: 2000, // increases limit from default 500 KB to 2000 KB
   },
 });
